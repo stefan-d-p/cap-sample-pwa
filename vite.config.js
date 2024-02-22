cb0f1e198ce33b4e44ecb1b80ebbaf42f689b52e
Stefan Weber <stefan.weber@spatium.one>
2024-02-22 17:11:39 +0200

    Added PWA features

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        theme_color: '#ffffff',
+        screenshots: [{
+          src: 'contacts-screen.png',
+          sizes: '388x833',
+        },{
+          form_factor: 'wide',
+          src: 'contacts-screen.png',
+          sizes: '388x833',
+        }],
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'  
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
+    mkcert()],
 })
